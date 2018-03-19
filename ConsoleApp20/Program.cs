4e70a74821dbe4c49c19e8e115765ae1cf0ef096
estimon <33309282+estimon@users.noreply.github.com>
2018-03-19 19:52:07 +0200

    xd

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Inimesed = new List<string>();
+            {
+                Inimesed.Add("Jenelle");
+                Inimesed.Add("Leonie");
+                Inimesed.Add("Bethanie");
+                Inimesed.Add("Breanna");
+                Inimesed.Add("Sidney");
+                Inimesed.Add("Kristyn");
+                Inimesed.Add("Kim");
+                Inimesed.Add("Guillermina");
+                Inimesed.Add("Fidela");
+                Inimesed.Add("Lora");
+            };
+            // Leiab nimede pikkused
+            var lenght = Inimesed.GroupBy(x => x.Length);
+
+            // teeb gruppid nime pikkuse järgi
+            foreach (var item in lenght)
+            {
+                
+                Console.WriteLine("Names with {0} letters:", item.Key);
+
+                // paneb nimed pikkuse järgi gruppidesse
+                foreach (var xd in item)
+                {
+                    
+                    Console.WriteLine("{0}", xd);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
